e9bd2a713e3fbac338d60caecc8a9877cafdf96d
Alexey A. Leonov <aaleonov@yandex.ru>
2017-06-15 21:47:51 +0300

    + checking downloadable files

---
+++ isDownloadable.ps1
@@ -0,0 +1,20 @@
+﻿[CmdletBinding()]
+Param(
+    $Storj
+)
+
+if (-not $storj) {$storj = '~\Storj.exe'}
+
+$temp = (Join-Path $env:Temp 1);
+(&$Storj list-buckets | sls "id: (.*?) ").matches | ? Success | % {
+    $bucketID = $_.Groups[1].Value; 
+    (&$Storj list-files $bucketID | sls "id: (.*?) ").matches | ? Success | %{
+        if (Test-Path $temp) {rm $temp}
+        $fileID = $_.Groups[1].Value; 
+        &$Storj download-file $bucketID $fileID $temp | %{
+            if ($_.Contains("error") -or $_.Contains("failed")) {
+                Write-Warning ($bucketID + " " + $fileID + ":" + $_);
+            }
+        }
+    }
+}
