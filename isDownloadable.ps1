33c6d14c3366e9a5c7a0739bbde20490872524e8
Alexey A. Leonov <aaleonov@yandex.ru>
2017-06-16 10:18:01 +0300

    + attempts to download on errors

--- isDownloadable.ps1
+++ isDownloadable.ps1
@@ -11,10 +11,17 @@ $temp = (Join-Path $env:Temp 1);
     (&$Storj list-files $bucketID | sls "id: (.*?) ").matches | ? Success | %{
         if (Test-Path $temp) {rm $temp}
         $fileID = $_.Groups[1].Value; 
-        &$Storj download-file $bucketID $fileID $temp | %{
-            if ($_.Contains("error") -or $_.Contains("failed")) {
-                Write-Warning ($bucketID + " " + $fileID + ":" + $_);
+        $attempt = 0;
+        do {
+            $errors = $null;
+            &$Storj download-file $bucketID $fileID $temp | %{
+                if ($_.Contains("error") -or $_.Contains("failed")) {
+                    Write-Warning ($bucketID + " " + $fileID + ": " + $_);
+                    $errors = $_;
+                    sleep 60;
+                }
             }
-        }
+            $attempt++;
+        } while ($errors -and $attempt -le 3)
     }
 }
